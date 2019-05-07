496691fb64719a0d7f741bb275ed433d5fa2559b
Agentkibble <btenney94@gmail.com>
2019-05-07 00:24:26 -0400

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,1 +1,9 @@
+Get-AppxPackage Microsoft.SkypeApp | Remove-AppxPackage
 
+Get-AppxPackage SpotifyAB.SpotifyMusic | Remove-AppxPackage
+
+Get-AppxPackage king.com.CandyCrushFriends | Remove-AppxPackage
+
+Get-AppxPackage king.com.CandyCrushSaga | Remove-AppxPackage
+
+Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
