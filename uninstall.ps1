eed0e074854ef7f1db8d3b7ce9d79cd590fca6e6
Agentkibble <btenney94@gmail.com>
2019-05-14 22:11:01 -0400

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -7,3 +7,25 @@ Get-AppxPackage king.com.CandyCrushFriends | Remove-AppxPackage
 Get-AppxPackage king.com.CandyCrushSaga | Remove-AppxPackage
 
 Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
+
+Get-AppxPackage 26720RandomSaladGamesLLC.SimpleSolitaire | Remove-AppxPackage
+
+Get-AppxPackage 4DF9E0F8.Netflix | Remove-AppxPackage
+
+Get-AppxPackage 5A894077.McAfeeSecurity | Remove-AppxPackage
+
+Get-AppxPackage 7EE7776C.LinkedInforWindows | Remove-AppxPackage
+
+Get-AppxPackage C27EB4BA.DropboxOEM | Remove-AppxPackage
+
+Get-AppxPackage Microsoft.OneConnect | Remove-AppxPackage
+
+Get-AppxPackage Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
+
+Get-AppxPackage Microsoft.Office.OneNote | Remove-AppxPackage
+
+Get-AppxPackage Microsoft.Office.Desktop | Remove-AppxPackage
+
+Get-AppxPackage B9ECED6F.ASUSGIFTBOX | Remove-AppxPackage
+
+Get-AppxPackage PricelinePartnerNetwork.Priceline.comTheBestDealso | Remove-AppxPackage
